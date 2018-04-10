c3fd80a74482b26cd44aa24434d0e2af275e9e92
xiaojiang <xiaojiang@DESKTOP-EIPSF1D>
2018-04-10 10:19:32 +0800

    app

---
+++ src/main/java/com/hongxing/aggregation/hongxing_agg/App.java
@@ -0,0 +1,13 @@
+package com.hongxing.aggregation.hongxing_agg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
